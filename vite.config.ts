c40437a21b87d3945e470e49d49fa66447b1de1c
S-Khraban <s.hraban@i.ua>
2026-01-03 22:32:22 +0200

    Fix Vite base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  base: '/todo-calendar-app/',
-  plugins: [
-    vue(),
-    UnoCSS(),
-  ],
+  base: '/',
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
